3adaf759740d7d71d2d392f933400b2a7ff117d5
Jiahao Chen <horychen@qq.com>
2020-05-30 19:57:41 +0800

    Move example to utility_json.py

--- utility_json.py
+++ utility_json.py
@@ -12,3 +12,26 @@ def from_json_recursively(fname, suffix=''):
         read_obj = jsonpickle.decode(f.read())
     return read_obj
 
+if __name__ == '__main__':
+
+    # build the obj
+    import module_winding_layout
+    wily_by_specs = module_winding_layout.winding_layout_v2(DPNV_or_SEPA=True, Qs=24, p=1, ps=2, coil_pitch_y=9)
+
+    import utility_json
+    # save as json
+    utility_json.to_json_recursively(wily_by_specs, 'test.json')
+    # load from json
+    wily = utility_json.from_json_recursively('test.json')
+
+    # test the obj
+    from pylab import plt, np
+    zQ = 100 # number of conductors/turns per slot
+    turns_per_layer = zQ / wily.number_winding_layer
+    U_phase = module_winding_layout.PhaseWinding(wily.Qs, wily.m, turns_per_layer, wily.ox_distribution_phase_U)
+    U_phase.plotFuncObj(U_phase.winding_func)
+    U_phase.plot2piFft(U_phase.winding_func, Fs=1/(2*np.pi/3600), L=65536*2**4) # 采样频率：在2pi的周期内取720个点
+    plt.show()
+
+
+
